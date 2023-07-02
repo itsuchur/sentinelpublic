013e559cb70f439a4056f65d6c0c754b41f16075
Dmitrii Efimov <itsuchur@itsuchur.dev>
2023-07-03 02:35:19 +0300

    Removed incorrect default value for boolean, got rid of USE command

--- migrations/V1__Initial_migration.sql
+++ migrations/V1__Initial_migration.sql
@@ -1,31 +1,38 @@
-CREATE ROLE sentinel WITH LOGIN PASSWORD 'password';
-CREATE DATABASE sentinel OWNER sentinel;
-USE sentinel;
+CREATE DATABASE sentinel;
+\c postgres
+CREATE USER sentinel WITH PASSWORD 'password';
+GRANT ALL PRIVILEGES ON DATABASE sentinel TO sentinel;
 
 CREATE TABLE IF NOT EXISTS t_actionlog (
     guild_id BIGINT NOT NULL,
     member_id BIGINT NOT NULL,
     action_type INTEGER,
     action_taken_at TIMESTAMP WITH TIME ZONE
-    );
+);
+
+ALTER TABLE t_actionlog OWNER TO sentinel;
 
 CREATE TABLE IF NOT EXISTS t_guilds (
     guild_id BIGINT PRIMARY KEY,
-    guild_autoban BOOLEAN FALSE,
+    guild_autoban BOOLEAN,
     guild_interval INTEGER,
     guild_hours INTEGER,
     guild_min_members INTEGER,
     guild_notification_channel BIGINT,
     guild_bot_present BOOLEAN,
     guild_url_filter BOOLEAN
-    );
+ );
+
+ALTER TABLE t_guilds OWNER TO sentinel;
 
 CREATE TABLE IF NOT EXISTS t_logs (
     id SERIAL PRIMARY KEY,
     guild_id BIGINT,
     raid_time TIMESTAMP WITH TIME ZONE,
     raiders_id BIGINT[]
-    );
+);
+
+ALTER TABLE t_logs OWNER TO sentinel;
 
 CREATE TABLE IF NOT EXISTS t_reports (
     id SERIAL PRIMARY KEY,
@@ -36,7 +43,9 @@ CREATE TABLE IF NOT EXISTS t_reports (
     search_from TIMESTAMP WITH TIME ZONE,
     search_until TIMESTAMP WITH TIME ZONE,
     found_ids BIGINT[]
-    );
+);
+
+ALTER TABLE t_reports OWNER TO sentinel;
 
 CREATE TABLE IF NOT EXISTS t_users (
     guild_id BIGINT,
@@ -46,4 +55,6 @@ CREATE TABLE IF NOT EXISTS t_users (
     member_left_at TIMESTAMP WITH TIME ZONE,
     is_whitelisted BOOLEAN,
     PRIMARY KEY (guild_id, member_id)
-    );
+);
+
+ALTER TABLE t_users OWNER TO sentinel;
